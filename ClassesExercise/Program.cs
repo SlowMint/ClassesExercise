2279a27846bd6242b8b3d9ce54ae58e95b2537a7
SlowMint <individualmotiv@gmail.com>
2022-07-17 17:06:20 -0500

    initial commit

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        public static void Main(string[] arg)
+        {
+            var pickUpTruck = new Car();
+            pickUpTruck.Make = "Toyota";
+            pickUpTruck.Model = "Tacoma";
+            pickUpTruck.Year = 2020;
+
+                Console.WriteLine($"Your truck is a {pickUpTruck.Year} {pickUpTruck.Make} {pickUpTruck.Model}");
+        }
+    }
+}
